6f5915308618bc353af0499774ee9ef67af357b1
Felix Anducho <felix.anducho74@gmail.com>
2022-10-31 18:52:30 -0600

    Create layouts for home and a generic one with just the navbar

---
+++ src/layouts/HomeLayout.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar/Navbar";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+export default function HomeLayout() {
+  return (
+    <main>
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+    </main>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import HomeLayout from "./layouts/HomeLayout";
+import BaseLayout from "./layouts/BaseLayout";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import "./styles/app.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <HomeLayout />,
+  },
+  {
+    path: "/item/:itemId",
+    element: <BaseLayout />,
   },
 ]);
 
