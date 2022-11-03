16558ee8e06048fe6acbe0105374e192edf133de
Felix Anducho <felix.anducho74@gmail.com>
2022-11-02 20:31:39 -0600

    Add link in every product and edit styles of links

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./ProductCard.module.css";
 
 interface ProductProps {
@@ -9,10 +10,12 @@ interface ProductProps {
 
 export default function ProductCard({ id, title, img, price }: ProductProps) {
   return (
-    <article key={id} className={styles.product_card}>
-      <h1 className={styles.product_card__title}>{title} </h1>
-      <img src={img} alt={title} height={120} width={80} />
-      <p className={styles.product_card__price}>$ {price}</p>
-    </article>
+    <Link to={`/item/${id}`} className={styles.product_link}>
+      <article key={id} className={styles.product_card}>
+        <h1 className={styles.product_card__title}>{title} </h1>
+        <img src={img} alt={title} height={120} width={80} />
+        <p className={styles.product_card__price}>$ {price}</p>
+      </article>
+    </Link>
   );
 }
