523a5bab967b78c288977956037e6b2b41e64d14
Felix Anducho <felix.anducho74@gmail.com>
2022-11-02 20:08:29 -0600

    Add styles to product card component

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,3 +1,5 @@
+import styles from "./ProductCard.module.css";
+
 interface ProductProps {
   id: number;
   title: string;
@@ -7,10 +9,10 @@ interface ProductProps {
 
 export default function ProductCard({ id, title, img, price }: ProductProps) {
   return (
-    <article key={id}>
-      <h1>{title} </h1>
+    <article key={id} className={styles.product_card}>
+      <h1 className={styles.product_card__title}>{title} </h1>
       <img src={img} alt={title} height={120} width={80} />
-      <p>$ {price}</p>
+      <p className={styles.product_card__price}>$ {price}</p>
     </article>
   );
 }
