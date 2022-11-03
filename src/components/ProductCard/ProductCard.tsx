128b9e6ca137268420364d6112871cced98a6b12
Felix Anducho <felix.anducho74@gmail.com>
2022-11-02 19:51:42 -0600

    Create product card component

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,16 @@
+interface ProductProps {
+  id: number;
+  title: string;
+  img: string;
+  price: number;
+}
+
+export default function ProductCard({ id, title, img, price }: ProductProps) {
+  return (
+    <article key={id}>
+      <h1>{title} </h1>
+      <img src={img} alt={title} height={120} width={80} />
+      <p>$ {price}</p>
+    </article>
+  );
+}
